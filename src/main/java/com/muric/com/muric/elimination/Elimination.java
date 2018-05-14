3239fe2c15b65944e4987bb01dbdd916c673fae5
Iouri Mouraviev <iv.mouraviev@verizon.net>
2018-05-14 12:12:29 -0400

    Fixing elimination logic and tests

--- src/main/java/com/muric/com/muric/elimination/Elimination.java
+++ src/main/java/com/muric/com/muric/elimination/Elimination.java
@@ -13,22 +13,23 @@ public class Elimination {
      * @return last element left after elimination
      */
     public static int[] eliminateEveryNelement(int[] array, int everyNth){
-        if(array.length == 0 || everyNth < 0){
+        if(array.length == 0 || everyNth < 1){
             throw new IllegalArgumentException("Cannot accept metod's arguments - array.length = " +
                 array.length + ", everyNth = " + everyNth);
         }
+        // Convert to queue
         Queue<Integer> queue = new LinkedList<>(Arrays.asList(Arrays.stream(array).boxed().toArray(Integer[]::new)));
 
-        if(everyNth <= queue.size() || queue.size() != 1){
-            int index = 0;
-            while(index <queue.size()){
+        if(everyNth < queue.size()){
+            int index = everyNth - 1;
+            while(index < queue.size()){
                 if(null != queue.peek()) {
-                    if (index < everyNth) {
+                    if (index < everyNth - 1) {
                         queue.add(queue.poll());
                         index++;
                     } else {
                         queue.remove();
-                        index = 0;
+                        index = everyNth - 1;
                     }
                 }
             }
--- src/test/java/com/muric/elimination/EliminationTest.java
+++ src/test/java/com/muric/elimination/EliminationTest.java
@@ -11,15 +11,17 @@ public class EliminationTest {
     private int[] empty = new int[0];
     private int[] ordered = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private int[] random = {-5, 7, 19, 11, -3, 55, 6, -88, 4, 7};
-    private int[] result489 = {4, 8, 9};
-    private int[] result16 = {1, 6};
-    private int[] resultRandom1 = {-5, 19, 11, 6};
-    private int[] resultRandom2 = {-5, -3, 55};
+    private int[] result10 = {10};
+    private int[] result910 = {9, 10};
+    private int[] result7 = {7};
+    private int[] resultRandom1 = {-88, 4, 7};
+    private int[] resultRandom2 = {4, 7};
 
     @DataProvider(name = "negative")
     Object[][] negativeData(){
         return new Object[][]{
             {empty, 1, null},       // error on empty array
+            {ordered, 0, null},     // error on Nth == 0
             {ordered, -3, null},    // error on negative Nth element
         };
     }
@@ -27,9 +29,11 @@ public class EliminationTest {
     @DataProvider(name = "elimination")
     Object[][] data(){
         return new Object[][]{
-            {ordered, 3, result489},
+            {ordered, 1, empty},
+            {ordered, 2, result10},
+            {ordered, 3, result910},
             {ordered, 12, ordered}, // nothing will be eliminated
-            {ordered, 2, result16},
+            {random, 2, result7},
             {random, 4, resultRandom1},
             {random, 3, resultRandom2}
         };
